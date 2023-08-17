97064d5040a3cd30d0c6487cb617dda5358f9da6
devsylva <sylvaejike@gmail.com>
2023-08-18 00:18:41 +0100

    omo

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
+// import all the necessary packages
 const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+// we are using port 8000
+const port = 8000;
+
+// we will create these todoRoutes in the future
+const todoRoutes = require("./routes/Todo");
+
 const app = express();
 
-// routes
-app.get("/", (req, res) => {
-  res.send("Hello DevTodo API");
-});
-
+// DB connection
 mongoose
   .connect(
     "mongodb+srv://devSylva:Ejike1451@cluster0.7vtfn.mongodb.net/?retryWrites=true&w=majority"
   )
   .then(() => {
-    console.log("connected to mongoose");
-    app.listen(3000, () => {
-      console.log("DevTodo API is running on port 3000");
-    });
-  })
-  .catch((error) => {
-    console.log(error);
+    console.log("CONNECTED TO DATABASE");
   });
+
+// middleware for cors to allow cross origin resource sharing
+app.use(cors());
+// middleware to convert our request data into JSON format
+app.use(bodyParser.json());
+
+// include the todoRoutes
+app.use("/api", todoRoutes);
+
+// start the server in the port 8000
+app.listen(port, () => {
+  console.log(`Listening to http://localhost:${port}`);
+});
